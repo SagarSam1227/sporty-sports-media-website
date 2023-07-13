85efbcfdb279622b5462a91ab1770689fbcbb148
Sagar <sagarsam606@gmail.com>
2023-07-13 10:32:19 +0530

    client created

---
+++ client/src/app.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Container=()=>{
+    return (
+        <div>
+        <h1>okkkk</h1>
+        </div>
+    )
+}
+
+
+ReactDOM.render(
+<React.StrictMode>
+    <Container />
+</React.StrictMode>,
+document.getElementById('root')
+)
