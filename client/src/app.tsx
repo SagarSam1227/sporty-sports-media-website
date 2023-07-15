dcfd965243b78463252ab94191697cb7d6445634
Sagar <sagarsam606@gmail.com>
2023-07-15 06:55:02 +0530

    login-signup

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LandingPage from './components/LandingPage/LandingPage'
+import Footer from './components/Layout/Footer/Footer'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home'
 
 function Container(){
   return (
     <>
     <LandingPage />
+    <Footer />
     </>
   )
 }
 
+const AppRouter = createBrowserRouter([
+{
+  path:"/",
+  element:<Container />,
+  children:[{
+    path:"/",
+    element:<Container />
+  },
+{
+  path:"/home",
+  element:<Home />
+}]
+}
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Container />
+    <RouterProvider router={AppRouter} />
   </React.StrictMode>,
 )
---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,7 @@
+function Home(){
+    return (
+        <div>homeee.......</div>
+    )
+}
+
+export default Home;
